d5a62347e3f8e3b08e765705c8c30f7f0d4c488e
codewithbear <lezhnin.vlad@gmail.com>
2025-06-17 13:45:20 +0000

    Morph: add console.log

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from 'express';
 
 export class HealthController {
-  constructor() {}
+  constructor() {
+    console.log('HealthController initialized');
+  }
 
   async getHealth(req: Request, res: Response): Promise<void> {
+    console.log('Handling getHealth request');
     res.status(200).json({ status: 'OK' });
     return;
   }
